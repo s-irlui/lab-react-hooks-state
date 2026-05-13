0f584a30086b1cc9369509a9099d805ff5ec6218
s-irlui <118243008+s-irlui@users.noreply.github.com>
2026-05-13 11:42:45 +0300

    Update ProductList to filter products
    Refactor ProductList to filter products by selected category.

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import ProductCard from './ProductCard'
+import ProductCard from './ProductCard';
+
 
 // Sample product data (for display purposes only)
 export const sampleProducts = [
@@ -7,15 +8,25 @@ export const sampleProducts = [
   { id: 2, name: 'Milk', price: '$2.50', category: 'Dairy', inStock: false }
 ]
 
-const ProductList = () => {
+const ProductList = ({selectedCategory, addToCart}) => {
+  //const[cart, setCart] = useState([]);
+  const filteredProducts = sampleProducts.filter((product) =>{ 
+     if (selectedCategory === "all" ){
+       return true
+     }
+    return product.category === selectedCategory})
+
   return (
     <div>
       <h2>Available Products</h2>
-
-      {/* TODO: Filter sample data using selected category */}
-      {sampleProducts.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+      {filteredProducts.length === 0 ?( 
+        <p>No products available.</p>
+       ): (
+      filteredProducts.map((product) => (
+        <ProductCard key={product.id} product={product} addToCart={addToCart}/>
+      ))
+      )}
+    
     </div>
   )
 }

886bda66ceb6212f60318b9ef31c66d18768b305
s-irlui <118243008+s-irlui@users.noreply.github.com>
2026-05-13 11:41:31 +0300

    Update DarkModeToggle.jsx

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -1,10 +1,14 @@
+//import { Button } from '@mui/material'
+//import Brightness4Icon from "@mui/icons-material/Brightness4";
+//import Brightness7Icon from "@mui/icons-material/Brightness7";
 import React from 'react'
 
-const DarkModeToggle = () => {
+const DarkModeToggle = ({mode, toggleMode}) => {
   // TODO: Implement dark mode toggle logic
 
   return (
-    <button>Toggle Dark Mode {/* TODO: Update this text from Dark to Light dynamically */}</button>
+   <button onClick={toggleMode}>
+    Toggle to {mode === "light" ? "Dark" : "Light"} Mode </button>
   )
 }
 
